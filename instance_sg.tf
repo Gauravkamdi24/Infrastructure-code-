deb71e4fa5be2a06af55333316b985a5e48b4908
Gauravkamdi24 <153709616+Gauravkamdi24@users.noreply.github.com>
2024-11-13 08:09:08 +0530

    Create instance_sg.tf

---
+++ instance_sg.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "sg" {
+  name        = "terraform_sg"
+  description = "Security group for Terraform EC2 instance"
+  vpc_id      = aws_vpc.vpc.id
+
+  # Allow inbound SSH (port 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow inbound HTTP (port 80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow outbound traffic
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "sg"
+  }
+}
