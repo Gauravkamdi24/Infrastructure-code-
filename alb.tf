9075d9bfdca761f28173a0da8f92aad6680060f3
Gauravkamdi24 <153709616+Gauravkamdi24@users.noreply.github.com>
2024-11-13 08:01:29 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -2,7 +2,7 @@
 resource "aws_lb" "myalb" {
   internal           = false
   load_balancer_type = "application"
-  subnets            = [aws_subnet.pvt_subnet-1.id, aws_subnet.public_subnet-3.d]
+  subnets            = [aws_subnet.pvt_subnet-1.id, aws_subnet.public_subnet-3.id]
 }
 
 resource "aws_lb_target_group" "mytg" {
